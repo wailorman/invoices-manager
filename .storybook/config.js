736022410691f244f5f6b93d220f423a43908f41
Sergey Popov <wailorman@gmail.com>
2018-06-23 17:29:41 +0300

    chore: travis

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,6 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   require('antd/dist/antd.css');
-  require('../src/stories');
   require('../src/stories/Auth.story');
   require('../src/stories/InvoiceList.story');
   require('../src/stories/InvoiceEdit.story');
