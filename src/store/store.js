2aa87bca53bef8752265def1457771ed95edd706
Sergey Popov <wailorman@gmail.com>
2018-06-23 17:44:46 +0300

    feat: Move to hashHistory

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history';
 
 import { rootReducer } from '../reducers/root-reducer';
 import { initialState } from './initial-state';
 
-export const history = createBrowserHistory();
+export const history = createHashHistory();
 
 export const store = createStore(
   connectRouter(history)(rootReducer),
