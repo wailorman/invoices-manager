25364e450863fa15494385cbd21e65aa3fa6fbdf
Sergey Popov <wailorman@gmail.com>
2018-06-25 11:36:01 +0300

    fix(store): Safari compose

--- src/store/store.js
+++ src/store/store.js
@@ -12,9 +12,9 @@ export const store = createStore(
   connectRouter(history)(rootReducer),
   initialState,
   compose(
-    applyMiddleware(routerMiddleware(history), thunk),
     // eslint-disable-next-line no-underscore-dangle
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(routerMiddleware(history), thunk),
   ),
 );
 
