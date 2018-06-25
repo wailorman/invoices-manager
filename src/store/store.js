5278bf3a01e3ea187723195355e06304bb63268a
Sergey Popov <wailorman@gmail.com>
2018-06-25 11:46:45 +0300

    fix(store): Comment redux dev tools cause of Safari...

--- src/store/store.js
+++ src/store/store.js
@@ -13,7 +13,7 @@ export const store = createStore(
   initialState,
   compose(
     // eslint-disable-next-line no-underscore-dangle
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(routerMiddleware(history), thunk),
   ),
 );
